9e05ddf495bdbfcbb0e635c3a76d7e4cb6d4416c
Israel Berlanga <israel0688@hotmail.com>
2020-04-05 14:05:45 -0500

    working on addEventListener

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,35 @@
 
 const characterLength = document.getElementById
 ('characterLength')
+const upperCaseyes = document.getElementById
+('upperCaseyes')
+const lowerCaseyes = document.getElementById
+('lowerCaseyes')
+const numberyes = document.getElementById
+('numberyes')
+const specialCyes = document.getElementById
+('specialCyes')
 
-const form = document.getElementById('passwordGeneratorForm')
+
+const form = document.getElementById
+ ('passwordGeneratorForm')
+
+
+
+
+form.addEventListener('submit', e =>
+{
+  e.preventDefault()
+  const characterLength = characterLength.value
+  const upperCaseyes = upperCaseyesElement.value
+  const lowerCaseyes = lowerCaseyesElement.value
+  const numberyes = numberyesElement.value
+  const specialCyes = specialCyesElement.value
+  const password = generatePassword(characterLength, upperCaseyes, lowerCaseyes, numberyes, specialCyes)
+
+})
+
+ /*
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -19,4 +46,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); */
