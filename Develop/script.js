c7a6edc057b73679fee4aa874e1a6ef09d42686e
Israel Berlanga <israel0688@hotmail.com>
2020-04-05 14:57:23 -0500

    arrays and .concat

--- Develop/script.js
+++ Develop/script.js
@@ -15,8 +15,28 @@ const specialCyes = document.getElementById
 const form = document.getElementById
  ('passwordGeneratorForm')
 
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47) 
+                            .concat(arrayFromLowToHigh(58, 64))
+                            .concat(arrayFromLowToHigh(91,96))
+                            .concat(arrayFromLowToHigh(124,126))
 
+function generatePassword(characterLength, upperCaseyes, lowerCaseyes, numberyes, specialCyes)
+{
 
+}
+
+function arrayFromLowToHigh(low, hight)
+{
+  const array = []
+  for (let i = low; i <= hight; i++)
+  {
+    array.push(i)
+  }
+  return array
+}
 
 form.addEventListener('submit', e =>
 {
